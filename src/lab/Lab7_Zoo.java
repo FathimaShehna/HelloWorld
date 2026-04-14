785f183a668d720c513813d961a87fda380543ad
Fathima Shehna <shehnafathima3@gmail.com>
2026-04-14 13:00:23 +0530

    class implemets interface

---
+++ src/lab/Lab7_Zoo.java
@@ -0,0 +1,44 @@
+package lab;
+
+	interface Animal{
+		public void makeSound();
+		public void move();
+	}
+		
+	 class Bird implements Animal{
+		
+		public void makeSound() {
+			System.out.println("Chirp");
+		}
+		
+		public void move() {
+			System.out.println("Fly");
+		}
+	}
+	
+	 class Dog implements Animal{
+		
+		public void makeSound() {
+			System.out.println("Bark");
+		}
+		
+		public void move() {
+			System.out.println("Run");
+		}
+	}
+	
+	public class Lab7_Zoo {
+		
+	public static void main(String[] args) {
+		
+		Bird b = new Bird();
+		Dog d = new Dog();
+		
+		b.makeSound();
+		b.move();
+		d.makeSound();
+		d.move();
+		
+	}
+
+}
