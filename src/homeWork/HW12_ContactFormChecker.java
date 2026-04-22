66c4c06e1196c75b4fb43f690230f444a13020d4
Fathima Shehna <shehnafathima3@gmail.com>
2026-04-22 09:58:37 +0530

    validating the user inputs

--- src/homeWork/HW12_ContactFormChecker.java
+++ src/homeWork/HW12_ContactFormChecker.java
@@ -1,8 +1,35 @@
 package homeWork;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HW12_ContactFormChecker {
+	
+	public static boolean isValidName(String name) {
+		
+	//	Pattern pattern = Pattern.compile("[A-Z][a-zA-Z]*");
+	//	Matcher matcher = pattern.matcher(name);
+	//	return matcher.find();                         //cant use matcher.find(); since it checks if any part of the string matches the pattern — not the whole string. 
+		return name.matches("[A-Z][a-zA-Z]*");         //so this is used
+	}
+	
+	public static boolean isValidEmail (String email) {
+		
+		if (email.startsWith("@") || email.endsWith("@") ||
+	            email.startsWith(".") || email.endsWith(".")) {
+	            return false;
+	        }
+	        return email.contains("@") && email.contains(".");
+	}
+	
+	public static boolean isValidPhNo (String phone) {
+		
+		Pattern pattern = Pattern.compile("\\d{10}");
+		Matcher matcher = pattern.matcher(phone);
+		return matcher.find();
+		
+	}
 
 	public static void main(String[] args) {
 		
@@ -15,10 +42,31 @@ public class HW12_ContactFormChecker {
 	        String email = sc.nextLine();
 
 	        System.out.print("Enter Phone Number: ");
-	        int phone = sc.nextInt();
+	        String phone = sc.nextLine();
 	        
+	        if(isValidName(name)) {
+	        	System.out.println("Name is valid");
+	        }
+	        else {
+	        	System.out.println("Name is not valid");
+	        }
 	        
-
+	        if(isValidEmail(email)) {
+	        	System.out.println("Email is valid");
+	        }
+	        else {
+	        	System.out.println("Email is not valid");
+	        }
+	        
+	        if(isValidPhNo(phone)) {
+	        	System.out.println("Phone number is valid");
+	        }
+	        else {
+	        	System.out.println("Phone number is not valid");
+	        }
+	        
+	        sc.close();
+	     
 	}
 
 }
